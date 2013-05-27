8098c84a05b6d09a6fe8cb41bfecd06b72c082a6
Kevin <jyt0532@gmail.com>
2013-05-27 13:35:03 +0800

    change click to mouseenter

--- popbox.js
+++ popbox.js
@@ -5,8 +5,7 @@
       selector      : this.selector,
       open          : '.open',
       box           : '.box',
-      arrow         : '.arrow',
-      arrow_border  : '.arrow-border',
+      hover_image   : '.hover-image',
       close         : '.close'
     }, options);
 
@@ -16,41 +15,21 @@
 
         var pop = $(this);
         var box = $(this).parent().find(settings['box']);
-
-        box.find(settings['arrow']).css({'left': box.width()/2 - 10});
-        box.find(settings['arrow_border']).css({'left': box.width()/2 - 10});
-
-        if(box.css('display') == 'block'){
-          methods.close();
-        } else {
-          box.css({'display': 'block', 'top': 10, 'left': ((pop.parent().width()/2) -box.width()/2 )});
-        }
+        var hover_image = $(this).find(settings['hover_image']);
+        box.css({'display': 'block', 'top': 92, 'left': 83});
+        hover_image.css({'display': 'block'})
       },
 
-      close: function(){
+      close: function(event){ 
+        event.preventDefault();
         $(settings['box']).fadeOut("fast");
+        $(settings['hover_image']).css({'display':'none'});
       }
     };
 
-    $(document).bind('keyup', function(event){
-      if(event.keyCode == 27){
-        methods.close();
-      }
-    });
-
-    $(document).bind('click', function(event){
-      if(!$(event.target).closest(settings['selector']).length){
-        methods.close();
-      }
-    });
-
     return this.each(function(){
-      $(this).css({'width': $(settings['box']).width()}); // Width needs to be set otherwise popbox will not move when window resized.
-      $(settings['open'], this).bind('click', methods.open);
-      $(settings['open'], this).parent().find(settings['close']).bind('click', function(event){
-        event.preventDefault();
-        methods.close();
-      });
+      $(settings['open'], this).bind('mouseenter', methods.open);
+      $(settings['open'], this).bind('mouseleave', methods.close);
     });
   }
 
